a4e95e37d0ab72a12849b8a72ec0ab555ec4b208
Joseph Soukup <u17.426@pslib.cloud>
2021-03-11 21:43:36 +0100

    basic layout

---
+++ FakeGimp/View/MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FakeGimp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void load_image(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zesileni_cerveny(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zesileni_zeleny(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zesileni_modre(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void odstiny_cervene(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void odstiny_zelene(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void odstiny_modre(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void odstiny_sede(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sepie(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void negativ(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void redukce_1_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void redukce_1_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void redukce_1_8(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rozostreni(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void vratit_zmeny(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
